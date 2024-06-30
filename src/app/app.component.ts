87f808065e6fc0dd92d3ed8c3bf7857d19e5589e
Emeka-JR <emekaa919@gmail.com>
2024-06-30 15:44:04 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { error } from 'console';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Observables';
+
+  data: any[] = [];
+
+  //1.Create an Observable
+  myObservable = new Observable((observer) => {
+    setTimeout(() => { observer.next(1) }, 1000);
+    setTimeout(() => { observer.next(2) }, 2000);
+    setTimeout(() => { observer.next(3) }, 3000);
+    // setTimeout(() => { observer.error(new Error('Something went wrong.Please try again later!')) }, 3000);
+    setTimeout(() => { observer.next(4) }, 4000);
+    setTimeout(() => { observer.next(5) }, 5000);
+    setTimeout(() => { observer.complete() }, 6000);
+  });
+
+  GetAsyncData() {
+
+    //Observer
+    //next,error,complete
+    this.myObservable.subscribe((val: any) => {
+      this.data.push(val);
+    },
+      (err) => {
+        alert(err.message)
+      },
+    ()=>{
+      alert('All the data is streamed!')
+    });
+  }
 }
